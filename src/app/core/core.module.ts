ac0c22ed996a03976797e23df527891b75096503
Mathieu Montjarret <mathieu.montjarret@icloud.com>
2022-08-18 13:18:21 +0200

    feat: CoreModule import on AppModule and exports on CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiModule } from '../ui/ui.module';
+import { IconsModule } from '../icons/icons.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { LoginModule } from '../login/login.module';
 
 
 
@@ -7,6 +11,7 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [UiModule, IconsModule, TemplatesModule, LoginModule]
 })
 export class CoreModule { }

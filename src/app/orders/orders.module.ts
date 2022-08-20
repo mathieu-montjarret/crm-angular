624b6597d8ca88fdbd39e799c9ee5f9bb9589d5b
Mathieu Montjarret <mathieu.montjarret@icloud.com>
2022-08-20 08:29:02 +0200

    feat: added view components

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrdersRoutingModule } from './orders-routing.module';
+import { PageListOrdersComponent } from './page-list-orders/page-list-orders.component';
+import { PageAddOrderComponent } from './page-add-order/page-add-order.component';
+import { PageEditOrderComponent } from './page-edit-order/page-edit-order.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageListOrdersComponent,
+    PageAddOrderComponent,
+    PageEditOrderComponent
+  ],
   imports: [
     CommonModule,
     OrdersRoutingModule

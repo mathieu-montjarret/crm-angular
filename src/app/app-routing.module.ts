57a7bf94efc57439156a87c1002f70718acef554
Mathieu Montjarret <mathieu.montjarret@icloud.com>
2022-08-24 01:04:37 +0200

    feat: added orders, clients, login and page not found paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,8 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { PageForgotPasswordComponent } from './login/page-forgot-password/page-forgot-password.component';
-import { PageResetPasswordComponent } from './login/page-reset-password/page-reset-password.component';
-import { PageSignInComponent } from './login/page-sign-in/page-sign-in.component';
-import { PageSignUpComponent } from './login/page-sign-up/page-sign-up.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
-  { path: 'sign-in', component: PageSignInComponent },
-  { path: 'sign-up', component: PageSignUpComponent },
-  { path: 'reset', component: PageResetPasswordComponent },
-  { path: 'forgot', component: PageForgotPasswordComponent },
   {
     path: 'orders',
     loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule)

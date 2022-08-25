c4c4ef9355adce54e22bcab1859d8ebc683e0620
Mathieu Montjarret <mathieu.montjarret@icloud.com>
2022-08-25 13:16:37 +0200

    feat: observables, subjects and behaviorSubjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crm-angular';
+
+  private obs = new Observable((listXsubscribe) => {
+    listXsubscribe.next(Math.random());
+  });
+
+  private subj = new Subject();
+
+  private behave = new BehaviorSubject(0);
+
+  constructor(){
+    this.obs.subscribe((data) => console.log(data));
+    this.obs.subscribe((data) => console.log(data));
+
+    this.subj.subscribe((data) => console.log(data));
+    this.subj.next('test');
+    this.subj.next('test 2');
+    this.subj.subscribe((data) => console.log(data));
+    this.subj.next(Math.random());
+    this.subj.subscribe((data) => console.log(data));
+
+    this.behave.subscribe((data) => console.log(data));
+    this.behave.next(1);
+    this.behave.subscribe((data) => console.log(data));
+    this.behave.next(2);
+    this.behave.next(Math.random());
+    this.behave.subscribe((data) => console.log(data));
+ 	  this.behave.subscribe((data) => console.log(data));
+}
+
 }
